bd66e2447dbf6f115f29b2a4a967d25e7a052849
ayaankumrah123 <76741090+ayaankumrah123@users.noreply.github.com>
2021-03-21 11:51:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var fairy, fairy1, fairy2;
+var star, starnight;
+
+var fairy=createSprite(200,20,30,20);
+var fairy1 =createSprite(180,60,30,20);
+var fairy2 =createSprite(215,40,30,20);
+  
+
+
+function preload()
+
+{
+  fairy=fairy.png ;
+  fairy1=fairy1.png ;
+  fairy2=fairy2.png ;
+  star=star.png ;
+  starnight=starnight.png ;
+
+  
+{
+
+function setup() {
+	createCanvas(800, 750);
+}
+
+
+function draw() {
+  background("black");
+
+}
